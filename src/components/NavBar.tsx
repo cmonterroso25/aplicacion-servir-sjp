c69f187a83defab127526df45fc5cd4d81d2cee5
cmonterroso25 <monterrosocarlos87@gmail.com>
2026-07-06 20:17:29 -0600

    Asignar seguimiento nuevamente

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -53,6 +53,13 @@ export default function NavBar({ rol }: Props) {
       )
     },
     {
+      label: 'Seguimientos', href: '/seguimientos', icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
+        </svg>
+      )
+    },
+    {
       label: 'Calendario', href: '/calendario', icon: (
         <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
